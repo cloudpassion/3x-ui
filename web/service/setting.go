8f17143fe84b74d19f85e6a300eac699d790398e
Kolos Dmitriy <2052756+cloudpassion@users.noreply.github.com>
2026-04-08 00:57:27 +0300

    Update setting.go

--- web/service/setting.go
+++ web/service/setting.go
@@ -60,7 +60,7 @@ var defaultValueMap = map[string]string{
 	"subEnableRouting":            "true",
 	"subRoutingRules":             "",
 	"subListen":                   "",
-	"subPort":                     "443",
+	"subPort":                     "2096",
 	"subPath":                     "/sub/",
 	"subDomain":                   "",
 	"subCertFile":                 "",
