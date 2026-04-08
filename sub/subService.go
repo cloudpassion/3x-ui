feff252903c3f608bbb9e175450472029a1e8b44
Kolos Dmitriy <2052756+cloudpassion@users.noreply.github.com>
2026-04-08 05:37:11 +0300

    Update subService.go

--- sub/subService.go
+++ sub/subService.go
@@ -452,7 +452,7 @@ func (s *SubService) genVlessLink(inbound *model.Inbound, email string) string {
 				//shortIds, _ := sidValue.([]any)
 				//params["sid"] = shortIds[random.Num(len(shortIds))].(string)
 				u := strings.Split(uuid, "-")
-				params["sid"] = fmt.Sprintf("%s322", strings.Join(u, ""))
+				params["sid"] = fmt.Sprintf("%s322", strings.Join(u[len(u)-1], ""))
 				
 			}
 			if fpValue, ok := searchKey(realitySettings, "fingerprint"); ok {
