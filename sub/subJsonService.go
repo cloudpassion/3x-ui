bd7386830747e7517e8819407e1adbd45d1dcb6c
Kolos Dmitriy <2052756+cloudpassion@users.noreply.github.com>
2026-04-08 05:06:52 +0300

    Update subJsonService.go

--- sub/subJsonService.go
+++ sub/subJsonService.go
@@ -277,7 +277,7 @@ func (s *SubJsonService) realityData(rData map[string]any, client model.Client) 
 	//	rltyData["shortId"] = ""
 	//}
 	u := strings.Split(client.ID, "-")
-	rltyData["shortId"] = u + "322"
+	rltyData["shortId"] = string(u) + "322"
 	serverNames, ok := rData["serverNames"].([]any)
 	if ok && len(serverNames) > 0 {
 		rltyData["serverName"] = serverNames[random.Num(len(serverNames))].(string)
