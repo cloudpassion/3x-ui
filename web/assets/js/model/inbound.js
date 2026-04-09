8d00bdee15f5a891da1b550d7e6c3f6b49e5371b
Kolos Dmitriy <2052756+cloudpassion@users.noreply.github.com>
2026-04-09 22:38:24 +0300

    Update inbound.js

--- web/assets/js/model/inbound.js
+++ web/assets/js/model/inbound.js
@@ -1736,8 +1736,13 @@ class Inbound extends XrayCommonClass {
     }
 
     genAllLinks(remark = '', remarkModel = '-ieo', client) {
+        console.log('t1');
         console.log(this);
-        console.log(settings = this._getDefaultSettings(type, settings));
+        console.log('t2');
+        console.log(this._getDefaultSettings(type, settings));
+        console.log('t3');
+        
+        
         let result = [];
         let email = client ? client.email : '';
         //let addr = !ObjectUtil.isEmpty(this.listen) && this.listen !== "0.0.0.0" ? this.listen : location.hostname;
